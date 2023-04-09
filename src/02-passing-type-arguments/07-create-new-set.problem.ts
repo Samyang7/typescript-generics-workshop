08173e60e804d50f064ce90d91f76a4d9c8b0d26
Samyang7 <samyangxh@gmail.com>
2023-04-10 01:38:40 +1000

    finished 02 exercise

--- src/02-passing-type-arguments/07-create-new-set.problem.ts
+++ src/02-passing-type-arguments/07-create-new-set.problem.ts
@@ -1,15 +1,16 @@
-import { Equal, Expect } from "../helpers/type-utils";
+import { Equal, Expect } from '../helpers/type-utils'
 
-export const createSet = () => {
-  return new Set();
-};
+// My solution
+export const createSet = <T>() => {
+  return new Set<T>()
+}
 
-const stringSet = createSet<string>();
-const numberSet = createSet<number>();
-const unknownSet = createSet();
+const stringSet = createSet<string>()
+const numberSet = createSet<number>()
+const unknownSet = createSet()
 
 type tests = [
   Expect<Equal<typeof stringSet, Set<string>>>,
   Expect<Equal<typeof numberSet, Set<number>>>,
-  Expect<Equal<typeof unknownSet, Set<unknown>>>,
-];
+  Expect<Equal<typeof unknownSet, Set<unknown>>>
+]
